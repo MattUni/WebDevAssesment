d14b5b61c011b2fb771151cf115fc3193c53ea2d
P17184090 <P17184090@my365.dmu.ac.uk>
2020-03-11 11:01:45 +0000

    Added a test version of the item list

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,24 @@
-const menuToggle=document.getElementById('menu-toggle')
-const menuNav=document.getElementById('menu-nav')
+const menuToggle=document.getElementById('menu-toggle');
+const menuNav=document.getElementById('menu-nav');
+const submitButton=document.getElementById('submit');
+const gothType=document.getElementById('type');
+const list1=document.getElementById('testlist');
+
 
 toggleMenu=()=>{
-  console.log("called toggleMenu")
-  menuNav.classList.toggle("menu-toggle")
+  console.log("called toggleMenu");
+  menuNav.classList.toggle("menu-toggle");
+};
+
+typeGet=()=>{
+  return gothType.options[gothType.selectedIndex].value;
+};
+
+function addItem (item){
+  const itemElement = document.createElement('li');
+  itemElement.textContent = item;
+  list1.appendChild(itemElement);
 }
 
-menuToggle.addEventListener("click",toggleMenu)
+menuToggle.addEventListener("click",toggleMenu);
+submitButton.addEventListener("click", addItem);

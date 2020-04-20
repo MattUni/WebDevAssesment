dd5a03d3b02a6a8e505600f35c32057ffbc90a79
MattUni <P17184090@my365.dmu.ac.uk>
2020-04-20 18:11:00 +0100

    trying to make the list save to DOM

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,7 @@
 const menuToggle=document.getElementById('menu-toggle');
 const menuNav=document.getElementById('menu-nav');
 const submitButton=document.getElementById('submit');
+const clearButton=document.getElementById('clearAll');
 const gothType=document.getElementById('type');
 const itemName=document.getElementById('itemname');
 const list1=document.getElementById('testlist');
@@ -19,10 +20,51 @@ typeGet=()=>{
 function addItem(item){
   const itemElement = document.createElement('li');
   itemElement.textContent = itemName.value;
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent= 'x';
+  itemElement.appendChild(deleteButton);
+  deleteButton.addEventListener('click', ev =>{
+    list1.removeChild(itemElement);
+  });
   list1.appendChild(itemElement);
-  console.log(itemElement);
-  console.log(itemName.value);
+}
+
+function allClear(){
+  while (list1.firstChild) {
+    list1.removeChild(list1.firstChild);
+  }
+}
+function renderList(list) {
+  list.forEach(addItem);
 }
 
 menuToggle.addEventListener("click", toggleMenu);
-submitButton.addEventListener("click", addItem);
+
+submitButton.addEventListener("click", ev => {
+  if(itemName.value){
+    addItem();
+    itemName.value=null;
+  }
+});
+
+clearButton.addEventListener("click", allClear);
+
+window.addEventListener('beforeunload', ev =>{
+  const items = [...list1.childNodes];
+  if(items.length){
+    const list = items.map(item=> {
+      return items.textContent.slice(0, -1);
+    });
+    localStorage.setItem('test-list', list.join('\n'));
+  } else{
+    localStorage.removeItem('test-list');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', ev =>{
+  const testlist = localStorage.getItem('test-list');
+  console.log(testlist);
+  if(testlist){
+    renderList(testlist.split(','));
+  }
+});
